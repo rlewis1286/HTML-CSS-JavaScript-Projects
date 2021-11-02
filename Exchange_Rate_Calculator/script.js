30ca4ea9cd00531152c79bb8fb2bfd79a6fafdb8
rlewis1286 <55451357+rlewis1286@users.noreply.github.com>
2021-11-02 12:02:21 -0400

    Add files via upload

---
+++ Exchange_Rate_Calculator/script.js
@@ -0,0 +1,23 @@
+const currencyEl_one = document.getElementById('currency-one');
+const currencyEl_two = document.getElementById('currency-two');
+const amountEl_one = document.getElementById('amount-one');
+const amountEl_two = document.getElementById('amount-two');
+
+const rateEl = document.getElementById('rate');
+const swap = document.getElementById('swap');
+
+// Fetch exchange rates and update DOM
+function calculate() {
+    const currency_one = currencyEl_one.value;
+    const currency_two = currencyEl_two.value;
+
+    fetch(``);
+}
+
+// Event listeners
+currencyEl_one.addEventListener('change', calculate);
+amountEl_one.addEventListener('input', calculate);
+currencyEl_two.addEventListener('change', calculate);
+amountEl_two.addEventListener('input', calculate);
+
+calculate();
